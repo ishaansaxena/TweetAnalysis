277e22b4e95c415bc99b61d49ce4292960592d6b
Ishaan Saxena <mail@ishaansaxena.in>
2016-06-05 03:45:14 +0530

    Remove bloatware

--- analyse.py
+++ analyse.py
@@ -5,5 +5,8 @@ import json
 import pandas
 import matplotlib.pyplot as plotlib
 
+TWEET_DATA_PATH = "data/"
+TWEET_DATA = []
+
 if __name__ == '__main__':
-    # TODO: Write this.
+    # TODO: Write http://adilmoujahid.com/posts/2014/07/twitter-analytics/
