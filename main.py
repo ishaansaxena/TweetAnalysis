e8be2f6ea22006569de2f0252a639bae52da6d60
Ishaan Saxena <mail@ishaansaxena.in>
2016-06-30 20:45:04 +0530

    Fix month name

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     # Need to be passed to analyse/miner
     stock_ticker = sys.argv[1]
     param_list = sys.argv[2:]
-    file_name = "data_" + time.strftime("%d_%m_%Y")
+    file_name = "data_" + time.strftime("%d_%b_%Y")
 
     os.system("python miner.py " + file_name + " " + " ".join(param_list))
     os.system("python analyse.py " + file_name + " " + stock_ticker)
