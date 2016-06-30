829dba89ee9e894dfb596e94f8c28ede00fcb6c3
Ishaan Saxena <mail@ishaansaxena.in>
2016-06-30 20:56:40 +0530

    Fix main

--- main.py
+++ main.py
@@ -14,5 +14,11 @@ if __name__ == '__main__':
     param_list = sys.argv[2:]
     file_name = "data_" + time.strftime("%d_%b_%Y")
 
-    os.system("python miner.py " + file_name + " " + " ".join(param_list))
-    os.system("python analyse.py " + file_name + " " + stock_ticker)
+    try:
+        os.system("python miner.py " + file_name + " " + " ".join(param_list))
+    except Exception, e:
+        print "Error: %s.\n" % e
+    try:
+        os.system("python analyse.py " + file_name + " " + stock_ticker)
+    except Exception, e:
+        print "Error: %s.\n" % e
